bf0d53c585f784ceb0672a09a9bdb39615af19cc
Ashish Rana <aashishrana269@gmail.com>
2020-03-22 05:52:36 +0530

    Update video-face-detection.py

--- video-face-detection.py
+++ video-face-detection.py
@@ -5,7 +5,7 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # To capture video from webcam. 
 cap = cv2.VideoCapture(0)
- #To use a video file as input 
+#To give video as input 
 #cap = cv2.VideoCapture('video.mp4')
 
 while True:
