1224608ffa780f3da49f7590d6afe4cf8b20154e
Ashish Rana <aashishrana269@gmail.com>
2020-04-20 09:30:34 +0530

    Update image-face-detection.py

--- image-face-detection.py
+++ image-face-detection.py
@@ -1,5 +1,5 @@
 import cv2          
-# Load the cascade 
+# Load the cascade  
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
 img = cv2.imread('ronaldo.jpg')
